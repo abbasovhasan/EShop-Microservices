cbc484ca5686be93b7677297bfb208627f2fc7ea
Hasan Abbasov <abbasovhsn05@gmail.com>
2024-09-25 01:45:18 +0400

    CatalogAPI created, Modules Created, CQRS Command-Query-Handler started

---
+++ src/eshop-microservices/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.ComponentModel;
+
+namespace CatalogAPI.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, decimal Price ) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
