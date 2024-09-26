06a84ff7aec0540354401257ffa76c03bc4d82ae
Hasan Abbasov <abbasovhsn05@gmail.com>
2024-09-27 01:55:16 +0400

    Developed POST Endpoint with Carter

--- src/BuildingBlocks/BuildingBlocks/obj/Debug/net8.0/BuildingBlocks.AssemblyInfo.cs
+++ src/BuildingBlocks/BuildingBlocks/obj/Debug/net8.0/BuildingBlocks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc484ca5686be93b7677297bfb208627f2fc7ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildingBlocks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/eshop-microservices/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/eshop-microservices/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,37 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
 using System.ComponentModel;
 
 namespace CatalogAPI.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, decimal Price ) : IRequest<CreateProductResult>;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price ) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        //1.create Product entity from command object
+        //2.save to database
+        //3.return CreateProductResult result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+        //TODO
+        //2.save to database
+
+        //3.return CreateProductResult result
+
+
+        return new CreateProductResult(Guid.NewGuid());
+
         throw new NotImplementedException();
     }
 }
